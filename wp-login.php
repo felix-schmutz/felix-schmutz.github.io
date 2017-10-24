dbc8c342d9a0ba48df7ef1f58719a4d145ed1753
felix-schmutz <33064302+felix-schmutz@users.noreply.github.com>
2017-10-24 17:24:19 +0200

    Add files via upload

---
+++ wp-login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="fr-FR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="fr-FR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>CV Félix Schmutz &lsaquo; Se connecter</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='https://felix-schmutz.github.io/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.2' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://felix-schmutz.github.io/wp-content/uploads/2017/10/cropped-Logo-moi--32x32.png" sizes="32x32" />
+<link rel="icon" href="https://felix-schmutz.github.io/wp-content/uploads/2017/10/cropped-Logo-moi--192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://felix-schmutz.github.io/wp-content/uploads/2017/10/cropped-Logo-moi--180x180.png" />
+<meta name="msapplication-TileImage" content="https://felix-schmutz.github.io/wp-content/uploads/2017/10/cropped-Logo-moi--270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-fr-fr">
+		<div id="login">
+		<h1><a href="https://wpfr.net/" title="Propulsé par WordPress" tabindex="-1">CV Félix Schmutz</a></h1>
+	
+<form name="loginform" id="loginform" action="https://felix-schmutz.github.io/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Nom d’utilisateur ou adresse e-mail<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Mot de passe<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Se souvenir de moi</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Se connecter" />
+		<input type="hidden" name="redirect_to" value="https://felix-schmutz.github.io/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://felix-schmutz.github.io/wp-login.php?action=lostpassword">Mot de passe oublié ?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+/**
+ * Filters whether to print the call to `wp_attempt_focus()` on the login screen.
+ *
+ * @since 4.8.0
+ *
+ * @param bool $print Whether to print the function call. Default true.
+ */
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://felix-schmutz.github.io/">&larr; Retour vers CV Félix Schmutz</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
